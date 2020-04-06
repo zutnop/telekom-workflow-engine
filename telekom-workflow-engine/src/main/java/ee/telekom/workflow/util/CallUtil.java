030a3c7ba51c1c5a8800ab708dca371a283b3642
Kuido Kondike <37999062+kuido85@users.noreply.github.com>
2020-04-06 16:03:15 +0300

    feat: Improvings for error messages to show actual class behind proxy. (#20)
    feat: Improving error messages to show actual class behind the proxy.

--- telekom-workflow-engine/src/main/java/ee/telekom/workflow/util/CallUtil.java
+++ telekom-workflow-engine/src/main/java/ee/telekom/workflow/util/CallUtil.java
@@ -2,25 +2,41 @@ package ee.telekom.workflow.util;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+
+import org.springframework.aop.framework.Advised;
 
 import ee.telekom.workflow.graph.WorkflowException;
 
-public class CallUtil{
 
-    public static Object call( Object target, String methodName, Object[] arguments ){
-        Method method = MethodUtil.findMethod( target.getClass(), methodName, MethodUtil.getArgumentClasses( arguments ) );
-        try{
-            return method.invoke( target, arguments );
+public class CallUtil {
+
+    public static Object call(Object target, String methodName, Object[] arguments) {
+        Method method = MethodUtil.findMethod(target.getClass(), methodName, MethodUtil.getArgumentClasses(arguments));
+        String className = getTargetName(target);
+        try {
+            return method.invoke(target, arguments);
+        } catch (IllegalAccessException e) {
+            throw new WorkflowException("Invoking method '" + method.getName() + "' on class '" + className + "' failed", e);
+        } catch (IllegalArgumentException e) {
+            throw new WorkflowException("Invoking method '" + method.getName() + "' on class '" + className + "' failed", e);
+        } catch (InvocationTargetException e) {
+            throw new WorkflowException("Invoking method '" + method.getName() + "' on class '" + className + "' failed", e);
         }
-        catch( IllegalAccessException e ){
-            throw new WorkflowException( "Invoking method '" + method.getName() + "' on class '" + target.getClass().getName() + "' failed", e );
+    }
+
+    private static String getTargetName(final Object target) {
+        if (Proxy.isProxyClass(target.getClass())) {
+            if (target instanceof Advised) {
+                try {
+                    return ((Advised)target).getTargetSource().getTarget().getClass().getName();
+                } catch (Exception e) {
+                    // Nothing to do here.
+                }
+            }
+            return target.getClass().getName() + " (unresolved proxy)";
         }
-        catch( IllegalArgumentException e ){
-            throw new WorkflowException( "Invoking method '" + method.getName() + "' on class '" + target.getClass().getName() + "' failed", e );
-        }
-        catch( InvocationTargetException e ){
-            throw new WorkflowException( "Invoking method '" + method.getName() + "' on class '" + target.getClass().getName() + "' failed", e );
-        }
+        return target.getClass().getName();
     }
 
 }
