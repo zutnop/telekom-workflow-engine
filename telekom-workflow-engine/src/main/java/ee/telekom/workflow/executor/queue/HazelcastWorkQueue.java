3eea3ed5e3358db827a387ee464d0f32304e5831
Erko Hansar <erko.hansar@xolo.io>
2022-11-22 15:50:09 +0200

    Before releasing version 1.5.0

--- telekom-workflow-engine/src/main/java/ee/telekom/workflow/executor/queue/HazelcastWorkQueue.java
+++ telekom-workflow-engine/src/main/java/ee/telekom/workflow/executor/queue/HazelcastWorkQueue.java
@@ -3,6 +3,7 @@ package ee.telekom.workflow.executor.queue;
 import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
@@ -11,12 +12,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.hazelcast.collection.IQueue;
+import com.hazelcast.collection.ItemEvent;
+import com.hazelcast.collection.ItemListener;
 import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import com.hazelcast.core.IQueue;
-import com.hazelcast.core.ItemEvent;
-import com.hazelcast.core.ItemListener;
 
 import ee.telekom.workflow.core.common.WorkflowEngineConfiguration;
 import ee.telekom.workflow.core.workflowinstance.WorkflowInstanceService;
@@ -45,14 +46,15 @@ public class HazelcastWorkQueue implements WorkQueue{
             log.info( "Didn't find an existing Hazelcast instance by name " + hcInstanceName + ". Starting our own instance!" );
             Config hcConfig = new Config();
             // Unfortunately, using the following line does not yet apply during the Hazelcast
-            // initialization such that Hazelcast initalization log output ends up at stout.
-            // This propblem is resolved by setting a system property as shown below.
+            // initialization such that Hazelcast initialization log output ends up at stout.
+            // This problem is resolved by setting a system property as shown below.
             // hcConfig.setProperty( "hazelcast.logging.type", "slf4j" );
             System.setProperty( "hazelcast.logging.class", "com.hazelcast.logging.Slf4jFactory" );
             hcConfig.setProperty( "hazelcast.jmx", "true" );
             hcConfig.setProperty( "hazelcast.shutdownhook.enabled", "false" );
+            hcConfig.setProperty( "hazelcast.phone.home.enabled", "false" );
             hcConfig.setInstanceName( hcInstanceName );
-            hcConfig.getGroupConfig().setName( config.getClusterName() );
+            hcConfig.setClusterName( config.getClusterName() );
             hcConfig.getNetworkConfig().getJoin().getMulticastConfig().setEnabled( true );
             hcConfig.getNetworkConfig().getJoin().getMulticastConfig().setMulticastGroup( config.getClusterMulticastGroup() );
             hcConfig.getNetworkConfig().getJoin().getMulticastConfig().setMulticastPort( config.getClusterMulticastPort() );
@@ -109,7 +111,7 @@ public class HazelcastWorkQueue implements WorkQueue{
     public void awaitEmpty(){
         IsEmptyListener listener = new IsEmptyListener( getWorkQueue() );
         boolean includeItemValuesInNotificationEvents = false;
-        String registrationId = getWorkQueue().addItemListener( listener, includeItemValuesInNotificationEvents );
+        UUID registrationId = getWorkQueue().addItemListener( listener, includeItemValuesInNotificationEvents );
 
         while( !getWorkQueue().isEmpty() ){
             try{
@@ -121,7 +123,7 @@ public class HazelcastWorkQueue implements WorkQueue{
         getWorkQueue().removeItemListener( registrationId );
     }
 
-    private static class IsEmptyListener implements ItemListener<WorkUnit>{
+    private static class IsEmptyListener implements ItemListener<WorkUnit> {
 
         private final Object monitor = new Object();
         private final IQueue<WorkUnit> queue;
